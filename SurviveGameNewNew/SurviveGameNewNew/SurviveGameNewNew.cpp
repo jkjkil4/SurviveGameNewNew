214ef050718312744ca5a411fa4f6738d0457b29
jkjkil4 <52841865+jkjkil4@users.noreply.github.com>
2020-03-05 18:27:58 +0800

    解决了由于窗口拉伸而导致画面拉伸的问题

--- SurviveGameNewNew/SurviveGameNewNew/SurviveGameNewNew.cpp
+++ SurviveGameNewNew/SurviveGameNewNew/SurviveGameNewNew.cpp
@@ -1,10 +1,6 @@
 ﻿#include "utility.h"
 #include <cmath>
 
-#include <iostream>
-#include <sstream>
-#include <fstream>
-
 #define PI acos(-1)
 
 HWND g_hWnd = 0;
@@ -14,19 +10,29 @@ HINSTANCE g_hInstance = 0;
 LPDIRECT3D9 g_pD3D = nullptr;			//D3D的接口指针，为了创建设备指针
 LPDIRECT3DDEVICE9 g_pDevice = nullptr;	//D3D的设备指针，为了创建精灵指针
 LPD3DXSPRITE g_pSprite = nullptr;		//D3D的精灵指针，为了画图
+LPD3DXSPRITE g_pSpriteRender = nullptr;	//用来渲染到纹理
 LPDIRECT3DTEXTURE9 g_pTexture = nullptr;
+
+// 渲染到纹理
+BOOL isInit = false;
+IDirect3DTexture9* g_pRenderTexture = NULL;
+IDirect3DSurface9* g_pRenderSurface = NULL;
+
 INT t = 0;
-INT currentT = 0;
+INT resizeTime = timeGetTime();
 
-INT timeThreadLogic = 0;	//逻辑处理
-INT timeThreadRender = 0;	//渲染
+INT timeThreadLogic = 0;	//逻辑处理耗时
+INT timeThreadRender = 0;	//渲染耗时
 
+INT defWidth = 800;
+INT defHeight = 608;
 
 
 LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg,
 	WPARAM wParam, LPARAM lParam);
 
 VOID onInit() {
+
 	//创建D3D接口指针
 	g_pD3D = Direct3DCreate9(D3D_SDK_VERSION);
 	D3DDISPLAYMODE d3ddm;		//D3D显示模式结构体
@@ -41,6 +47,8 @@ VOID onInit() {
 
 	d3dpp.BackBufferCount = 1;	//后台缓冲区的个数（双缓冲技术）
 	d3dpp.Windowed = TRUE;
+	d3dpp.BackBufferWidth = defWidth;
+	d3dpp.BackBufferHeight = defHeight;
 	d3dpp.SwapEffect = D3DSWAPEFFECT_DISCARD;	//翻转效果：抛弃
 
 	//创建设备指针
@@ -55,9 +63,22 @@ VOID onInit() {
 
 	//创建精灵指针
 	D3DXCreateSprite(g_pDevice, &g_pSprite);
+	D3DXCreateSprite(g_pDevice, &g_pSpriteRender);
 
+	//载入纹理
 	D3DXIMAGE_INFO imageInfo;
-	myCreateTexture( g_pDevice, "texture\\test2.png", &imageInfo, &g_pTexture );
+	myCreateTexture( g_pDevice, "texture\\test.png", &imageInfo, &g_pTexture );
+
+	//"渲染到纹理"
+	g_pDevice->CreateTexture(
+		GetSystemMetrics(SM_CXFULLSCREEN), GetSystemMetrics(SM_CYFULLSCREEN), 1,
+		D3DUSAGE_RENDERTARGET,
+		D3DFMT_R5G6B5,
+		D3DPOOL_DEFAULT,
+		&g_pRenderTexture,
+		NULL);
+	//得到纹理的Surface
+	g_pRenderTexture->GetSurfaceLevel(0, &g_pRenderSurface);
 }
 INT onLogic() {
 	//得到逻辑处理开始时的时间
@@ -70,29 +91,40 @@ INT onLogic() {
 INT onRender() {
 	//得到绘制开始时的时间
 	int tStart = timeGetTime();
-	//D3DCOLOR_XRGB(102, 204, 255)
+
+	// 渲染到纹理
+	IDirect3DSurface9* g_pOldRenderTarget;
+	g_pDevice->GetRenderTarget(0, &g_pOldRenderTarget);
+	g_pDevice->SetRenderTarget(0, g_pRenderSurface);
 	g_pDevice->Clear(0, nullptr, D3DCLEAR_TARGET, D3DCOLOR_XRGB(102, 204, 255), 1.0f, 0);
-	g_pDevice->BeginScene();		//获取绘制权限
-	//---------------------------------------
-	//		渲染代码
-	g_pSprite->Begin(D3DXSPRITE_ALPHABLEND);//参数告知系统，需要alpha混合的支持
-	//Sprite的绘制函数
+	g_pSprite->Begin(D3DXSPRITE_ALPHABLEND);
 	g_pSprite->Draw(
 		g_pTexture,				//绘制哪张图片		
 		nullptr,	//绘制图片的哪个部分
 		&D3DXVECTOR3(0, 0, 0),	//图片的锚点
-		&D3DXVECTOR3(t/5.0f, 0, 0),	//绘制在客户区的哪个位置
+		&D3DXVECTOR3(0, 0, 0),	//绘制在客户区的哪个位置
 		D3DCOLOR_XRGB((int)(cos(t * PI / 180) * 127 + 127), (int)(sin(t * PI / 180) * 127 + 127), 255)//绘制混合色
 	);
-	g_pSprite->End();	//告诉系统，在3D的窗口中绘制2D的工作结束
-	//---------------------------------------
+	g_pSprite->End();
+
+	// 绘制纹理
+	g_pDevice->SetRenderTarget(0, g_pOldRenderTarget);
+	//g_pDevice->Clear(0, nullptr, D3DCLEAR_TARGET, D3DCOLOR_XRGB(102, 204, 255), 1.0f, 0);
+	g_pDevice->BeginScene();		//获取绘制权限
+	g_pSpriteRender->Begin(NULL);
+	g_pSpriteRender->Draw(g_pRenderTexture, nullptr, &D3DXVECTOR3(0, 0, 0),   //绘制函数
+		&D3DXVECTOR3(0, 0, 0), D3DCOLOR_XRGB(255, 255, 255));
+	g_pSpriteRender->End();
 	g_pDevice->EndScene();		//结束绘制
 	g_pDevice->Present(nullptr, nullptr, 0, nullptr);	//前后台缓冲区交换的"源动力"
-	return timeGetTime() - tStart;	//得到绘制消耗的时间
+
+	// 得到绘制消耗的时间
+	return timeGetTime() - tStart;	
 }
 VOID onDestroy() {
 	Safe_Release(g_pTexture);
 	Safe_Release(g_pSprite);
+	Safe_Release(g_pSpriteRender);
 	Safe_Release(g_pDevice);
 	Safe_Release(g_pD3D);
 }
@@ -111,7 +143,7 @@ VOID threadRender(bool* flag) {
 			break;
 		int currentTime = timeGetTime();
 		int elapsedTime = currentTime - timeThreadRender;
-		if (!IsIconic(g_hWnd)) {
+		if (!IsIconic(g_hWnd) && currentTime - resizeTime > 120 ) {
 			onRender();	//绘制
 		}
 		if (elapsedTime < 17)
@@ -141,15 +173,25 @@ INT WINAPI WinMain(__in HINSTANCE hInstance,
 	RegisterClass(&wc);
 
 	g_hWnd = CreateWindow(wc.lpszClassName,
-		TEXT("Linimass"), WS_OVERLAPPEDWINDOW,
-		50, 20, 800, 600, NULL, NULL, hInstance, NULL);
+		TEXT("Linimass"), WS_OVERLAPPEDWINDOW, 40, 20, 
+		GetSystemMetrics(SM_CXFULLSCREEN) - 80, GetSystemMetrics(SM_CYFULLSCREEN) - 40, 
+		NULL, NULL, hInstance, NULL);
+
+	//最大化
+	ShowWindow(g_hWnd, SW_SHOWMAXIMIZED);
+
+	//得到视野宽高
+	RECT rect;
+	GetClientRect(g_hWnd, &rect);
+	defWidth = rect.right - rect.left;
+	defHeight = rect.bottom - rect.top;
 
 	if (g_hWnd){
 		g_hInstance = hInstance;
 
 		onInit();
+		isInit = true;
 
-		ShowWindow(g_hWnd, SW_SHOWNORMAL);
 		UpdateWindow(g_hWnd);
 	}
 
@@ -184,6 +226,28 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg,
 	WPARAM wParam, LPARAM lParam)
 {
 	switch (uMsg) {
+	case WM_SIZE: {
+		if (isInit) {
+			resizeTime = timeGetTime();
+			D3DXMATRIX g_scale;
+			RECT rect;
+			GetClientRect(g_hWnd, &rect);
+			int w = rect.right - rect.left;
+			int h = rect.bottom - rect.top;
+			D3DXMatrixTransformation2D(
+				&g_scale,		//返回的矩阵
+				nullptr,		//缩放的中心
+				0.0f,			//影响缩放的因素
+				&D3DXVECTOR2((float)defWidth / w, (float)defHeight / h),	//在X、Y方向的缩放量
+				nullptr,	//旋转中心
+				0,			//旋转弧度
+				nullptr		//平移量
+			);
+			g_pSpriteRender->SetTransform(&g_scale);
+			D3DXMatrixIdentity(&g_scale);
+		}
+		break;
+	}
 	case WM_KEYDOWN:
 		if (wParam == VK_ESCAPE)
 			DestroyWindow(hWnd);
