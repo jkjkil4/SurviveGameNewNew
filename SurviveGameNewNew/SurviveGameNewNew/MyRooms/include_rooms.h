dfee1d900edcd4d826da2787a6e71cf16412bcf0
jkjkil4 <52841865+jkjkil4@users.noreply.github.com>
2020-06-17 18:55:25 +0800

    init

--- SurviveGameNewNew/SurviveGameNewNew/MyRooms/include_rooms.h
+++
@@ -1,5 +0,0 @@
-#include "MyRoom_title.h"
-#include "MyRoom_game.h"
-#include "MyRoom_createSave.h"
-#include "MyRoom_loadSave.h"
-#include "MyRoom_saveSave.h"
--- SurviveGameNewNew/SurviveGameNewNew/MyWidgets/MySolidColorWidget.cpp
+++
@@ -1,9 +0,0 @@
-#include "MySolidColorWidget.h"
-
-MySolidColorWidget::MySolidColorWidget(MyEngine* e, int w, int h, MyWidget* parent)
-	: MyWidget(e, w, h, nullptr, parent) {}
-
-inline void MySolidColorWidget::_onRender(LPD3DXSPRITE g_pSprite, int targetX, int targetY) {
-	e->drawRestart();
-	e->drawRect(targetX, targetY, w, h, color, color, color, color);
-}
